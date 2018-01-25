8b00daca63c02a69cdc1b1853a5e47710502a89e
Prithvi <Prithvisbisht@gmail.com>
2018-01-25 21:39:15 +0530

    change in bigendian and little endian

--- little endian and big endian.c
+++ little endian and big endian.c
@@ -1,6 +1,20 @@
 /* 
    Write a C program to find out if the underlying 
    architecture is little endian or big endian. 
+   int x = 0x76543210;
+char *c = (char*) &x;
+
+Big endian format:
+------------------
+Byte address  | 0x01 | 0x02 | 0x03 | 0x04 | 
+              +++++++++++++++++++++++++++++
+Byte content  | 0x76 | 0x54 | 0x32 | 0x10 |
+       
+Little endian format:
+---------------------
+Byte address  | 0x01 | 0x02 | 0x03 | 0x04 | 
+              +++++++++++++++++++++++++++++
+Byte content  | 0x10 | 0x32 | 0x54 | 0x76 |
  */
  
 #include <stdio.h>
